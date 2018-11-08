67a754d0b054dcdbfe4b993b3f93b8ac6c506b69
Alaa Zorkane <raralazoril@gmail.com>
2018-11-08 23:46:47 +0100

    init commit ( v0.5 )

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
+/* Libraries */
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
+/* Components */
+import Landing from './components/Landing'
+import Journey from './components/Journey'
+
+
+/* CSS & Assets */
+import './css/general/App.css'
+
+/* App - Handling routes & paths and loading into the dom */
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="container">
+          <Route exact path="/" component={Landing} />
+          <Route path="/Journey" component={Journey} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Journey.js
@@ -0,0 +1,21 @@
+/* Libraries */
+import React, { Component } from 'react';
+
+/* Components */
+
+
+/* CSS & Assets */
+import '../css/components/Journey.css'
+
+/* A Brief History of Time */
+class Journey extends Component {
+  render() {
+    return (
+        <div>
+            Journey WIP
+        </div>
+    );
+  }
+}
+
+export default Journey;
