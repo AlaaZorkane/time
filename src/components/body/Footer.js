8387c8e73f040f8f36f972771761cbb478374b66
Alaa Zorkane <raralazoril@gmail.com>
2018-11-18 22:02:28 +0100

    updated license to GPL v3

--- src/components/body/Footer.js
+++ src/components/body/Footer.js
@@ -13,7 +13,7 @@ class Footer extends Component {
             <div className="Footer">
                 <p className="cp">
                     Copyright <span aria-label="Alaeeee" className="alaa" onClick={ () => window.open("https://www.linkedin.com/in/AlaaZorkane")}>Alaa Zorkane</span> © 2018 <br />
-                    Protected under <span className="license" onClick= { () => window.open("https://spdx.org/licenses/ECL-2.0.html")}>ECL-2.0</span> <br/>
+                    Protected under <span className="license" onClick= { () => window.open("https://choosealicense.com/licenses/gpl-3.0/")}>GNU GPLv3</span> <br/>
                     <span className="emsi" onClick= { () => window.open("http://www.emsi.ma")}>EMSI orangers</span>
                 </p>
             </div >
