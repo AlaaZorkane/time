2834cee62334c468b87584babb2ad22f81414dc2
Alaa Zorkane <raralazoril@gmail.com>
2018-11-22 19:12:22 +0100

    added loader / responsive css

--- src/components/journey/Body.js
+++ src/components/journey/Body.js
@@ -9,6 +9,7 @@ import PersonPopup from './PersonPopup'
 /* CSS & Assets */
 import '../../css/components/journey/Body.css'
 import popup from '../../assets/sounds/popup.wav'
+import infinityloading from '../../assets/infinityloading.svg'
 
 /* JS */
 const soundEffects = [
@@ -35,11 +36,14 @@ const Body = ({event}) => {
     */
     return (
         <div className="Body">
+
             <div className="grid1">
+                {/* TITLE BLOCK */}
                 <div className="title span-col-3">
                     <h1>{title}</h1>
                 </div>
                 <div className="grid2">
+                    {/* INFOS BLOCK */}
                     <div>
                         <li>
                             <ul className="era"><span>Era</span><br/>{era}</ul>
@@ -47,38 +51,68 @@ const Body = ({event}) => {
                             <ul><span>Place</span><br/>{place}</ul>
                         </li>
                     </div>
+                    {/* VIDEO BLOCK */}
                     <div className="video-wrapper">
                         <iframe title={title} src={"https://www.youtube.com/embed/"+ytbID+"?autoplay=0&controls=0"} frameBorder="0" gesture="media" allow="encrypted-media"></iframe>
                     </div>
-                    <div className="gridalt">
+                    {/* PEOPLE BLOCK */}
+                    <div className="keyPeople gridalt">
+                        {/* P_TITLE BLOCK */}
                         <div className="span-col-2">
                             <h2>Key People</h2>
                         </div>
-                        <div>
-                            <img src={p1[1]} alt={p1[0]}/>
-                        </div>
-                        <div>
-                            <img src={p1[1]} alt={p1[0]}/>
-                        </div>
-                        <div className="span-col-3">
+                        {/* P_PICTURES BLOCK */}
+                        <Popup
+                            trigger={   <div className="picture-wrapper">
+                                        <img src={p1[1]} alt={p1[0]}/>
+                                        </div> }
+                            onOpen={ () => {soundEffects[0].play()} }
+                            onClose= { () => soundEffects[1].play() }
+                            modal> 
+                            {close => (
+                                <div className="MainPopup">
+                                    <a className="close" onClick={close}>
+                                        &times;
+                                    </a>
+                                    <PersonPopup name={p1[0]} desc={p1[2]}/>
+                                </div>
+                            )}
+                        </Popup>
+                        <Popup
+                            trigger={   <div className="picture-wrapper">
+                                            <img src={p1[1]} alt={p1[0]}/>
+                                        </div> }
+                            onOpen={ () => {soundEffects[0].play()} }
+                            onClose= { () => soundEffects[1].play() }
+                            modal> 
+                            {close => (
+                                <div className="MainPopup">
+                                    <a className="close" onClick={close}>
+                                        &times;
+                                    </a>
+                                    <PersonPopup name={p1[0]} desc={p1[2]}/>
+                                </div>
+                            )}
+                        </Popup>
+                        {/* P_TIP BLOCK */}
+                        <div className="span-col-2">
                             <p className="tip">
                                 Click on a picture<br/>to learn more
                             </p>
                         </div>
                     </div>
                 </div>
+                {/* FACTS BLOCK */}
                 <div className="grid3 span-col-3">
+                    {/* FACT CARD N1 BLOCK */}
                     <div className="card g1">
                         <h2>Fact 1</h2>
-                        <div className="text-area">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati officiis architecto ducimus minima accusamus repudiandae minus doloribus recusandae, sit magnam saepe hic fugit reprehenderit fugiat nisi ab, excepturi vitae non.</p> 
-                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati officiis architecto ducimus minima accusamus repudiandae minus doloribus recusandae, sit magnam saepe hic fugit reprehenderit fugiat nisi ab, excepturi vitae non.</p> 
                     </div>
+                    {/* FACT CARD N2 BLOCK */}
                     <div className="card g2">
                         <h2>Fact 2</h2>
-                        <div className="text-area">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati officiis architecto ducimus minima accusamus repudiandae minus doloribus recusandae, sit magnam saepe hic fugit reprehenderit fugiat nisi ab, excepturi vitae non.</p> 
-                        </div>   
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati officiis architecto ducimus minima accusamus repudiandae minus doloribus recusandae, sit magnam saepe hic fugit reprehenderit fugiat nisi ab, excepturi vitae non.</p>  
                     </div>
                 </div>
             </div>
