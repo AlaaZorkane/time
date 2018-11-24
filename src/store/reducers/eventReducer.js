3fafce8921b81a93a02505feda086b58f564cefc
Alaa Zorkane <raralazoril@gmail.com>
2018-11-24 21:39:30 +0100

    Added admin page for easier db manipulation

--- src/store/reducers/eventReducer.js
+++ src/store/reducers/eventReducer.js
@@ -1,7 +1,21 @@
 const initState = {}
 
 const eventReducer = (state = initState, action) => {
-  return state
+  switch (action.type) {
+    /* add event */
+    case 'ADD_EVENT':
+      console.log('Added the event successfully to the DB :)', action.event)
+      return state;
+
+    /* error handling */
+    case 'ADD_EVENT_ERROR':
+      console.log('in3l tbon mi ana', action.err)
+      return state;
+
+    /* default state */
+    default:
+      return state;
+  }
 }
 
 export default eventReducer;
